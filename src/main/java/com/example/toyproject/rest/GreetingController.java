cb95dd17ca8089c9c377fc1d17188b16feff5aef
Luca <incivileluca@gmail.com>
2025-09-21 23:25:59 +0200

    added request parameter and counter

--- src/main/java/com/example/toyproject/rest/GreetingController.java
+++ src/main/java/com/example/toyproject/rest/GreetingController.java
@@ -2,15 +2,23 @@ package com.example.toyproject.rest;
 
 import com.example.toyproject.domain.data.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 //marks class to offer interfaces/mappings
 @RestController
 public class GreetingController {
+    //static= bound              //%s gets replaced by passed variable
+    private static final String template = "Hello %s";
+                        //Atomic is thread safe
+    private static final AtomicLong counter = new AtomicLong();
 
     //Mapping tells on what path/method resource is available (e.g. localhost:8080/greeting)
     @GetMapping("/greeting")
-    public Greeting greeting() {
-        return new Greeting(1, "2");
+    //url parameter is accepted e.g. /greetings?user= if no Param is provived defaultValue is used
+    public Greeting greeting(@RequestParam(defaultValue = "Bob") String user) {
+        return new Greeting(counter.incrementAndGet(), user);
     }
 }
